3b9e8c3f7ad5c6d1a14021b63af188d59d7885ba
jurlaub <j.urlaub@gmail.com>
2015-07-20 12:13:21 -0700

    initial commit

---
+++ app/src/main/java/com/example/android/awesomesaucemovies/MainActivityFragment.java
@@ -0,0 +1,42 @@
+package com.example.android.awesomesaucemovies;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    private ArrayAdapter<String> mMovieAdapter;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        String[] dList = {"pepper", "Die Hard", "Right Round", "Holder", "Flo Rida", "Shawtie"};
+
+        mMovieAdapter = new ArrayAdapter<String>(this.getActivity(),R.layout.list_item_movie, R.id.list_item_movie_textview );
+
+        for(String s: dList) {
+            mMovieAdapter.add(s);
+        }
+
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listView);
+        listView.setAdapter(mMovieAdapter);
+
+        return rootView;
+    }
+}
