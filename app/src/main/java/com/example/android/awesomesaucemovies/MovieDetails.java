9c0bd48e2e76ddb0199a012ce52b247e76db0412
jurlaub <j.urlaub@gmail.com>
2015-07-31 15:53:17 -0700

    working on adding detail view

---
+++ app/src/main/java/com/example/android/awesomesaucemovies/MovieDetails.java
@@ -0,0 +1,22 @@
+package com.example.android.awesomesaucemovies;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by dev on 7/31/15.
+ */
+public class MovieDetails extends ActionBarActivity {
+
+    private final String LOG_TAG = MovieDetails.class.getSimpleName();
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+
+}
