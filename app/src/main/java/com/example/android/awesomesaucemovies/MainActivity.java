8a92d19114412919e7a69ccb01df4f1db4b98f80
jurlaub <j.urlaub@gmail.com>
2015-11-15 22:59:18 -0800

    Working on Two-pane view. Single pane works, twopane crashes. In the Twopane view, the  MovieDetailsFragment does not have a movieID in order to display data.

--- app/src/main/java/com/example/android/awesomesaucemovies/MainActivity.java
+++ app/src/main/java/com/example/android/awesomesaucemovies/MainActivity.java
@@ -1,29 +1,72 @@
 package com.example.android.awesomesaucemovies;
 
 import android.database.Cursor;
-import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 
 
-public class MainActivity extends SingleFragmentActivity implements MovieFragment.Callbacks {
+public class MainActivity extends ActionBarActivity implements MovieFragment.Callbacks {
 
-    @Override
-    protected Fragment createFragment(){
-        return new MovieFragment();
-    }
+    private static final String MOVIEDETAILFRAGMENT = "MDF_Tag";
 
-
-
-    @Override
-    protected int getLayoutResID(){
-
-        return R.layout.activity_masterdetail;
-    }
-
+    private boolean mTwoPane;
 
     public void onMovieDetailSelected(Cursor movieDetailCursor) {
 
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_masterdetail);
+
+        if(findViewById(R.id.fragment_detail) != null){
+            mTwoPane = true;
+
+            if (savedInstanceState == null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_detail, new MovieDetailsFragment(), MOVIEDETAILFRAGMENT )
+                        .commit();
+            }
+
+        } else {
+            mTwoPane = false;
+
+        }
+
+
+//        MovieFragment movieFragment = ((MovieFragment) getSupportFragmentManager()
+//            .findFragmentById(R.id.fragmentContainer));
+        //movieFragment.setUseTodayLayout(!mTwoPane);
+
+
+
+//        FragmentManager fm = getSupportFragmentManager();
+//        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+//
+//        if (fragment == null) {
+//            fragment = createFragment();
+//            fm.beginTransaction()
+//                    .add(R.id.fragmentContainer, fragment)
+//                    .commit();
+//        }
+    }
+
+
+
+    //    @Override
+//    protected Fragment createFragment(){
+//        return new MovieFragment();
+//    }
+
+
+
+//    @Override
+//    protected int getLayoutResID(){
+//
+//        return R.layout.activity_masterdetail;
+//    }
 
 
 }
--- app/src/main/java/com/example/android/awesomesaucemovies/MovieDetails.java
+++ app/src/main/java/com/example/android/awesomesaucemovies/MovieDetails.java
@@ -1,13 +1,15 @@
 package com.example.android.awesomesaucemovies;
 
 
-import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBarActivity;
 
 /**
  * Created by dev on 7/31/15.
  */
 
-public class MovieDetails extends SingleFragmentActivity {
+public class MovieDetails extends ActionBarActivity {
 
 
     public static final String TRAILERS = "trailers";
@@ -16,11 +18,53 @@ public class MovieDetails extends SingleFragmentActivity {
 
 
 
-    @Override
-    protected Fragment createFragment(){
-        return new MovieDetailsFragment();
 
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        if (savedInstanceState == null) {
+//
+//            // create detail fragment and add it to th activity
+//
+//            //Bundle arguments = new Bundle();
+//        Bundle arguments = new Bundle();
+//        arguments.putParcelable(DetailFragment.DETAIL_URI, getIntent().getData());
+//
+//        DetailFragment fragment = new DetailFragment();
+//        fragment.setArguments(arguments);
+//
+//
+//        }
+
+            FragmentManager fm = getSupportFragmentManager();
+           // Fragment fragment = fm.findFragmentById(R.id.fragment_detail);
+
+
+            MovieDetailsFragment fragment = new MovieDetailsFragment();
+                fm.beginTransaction()
+                        .add(R.id.fragment_detail, fragment)
+                        .commit();
+
+        }
     }
 
+
+
+//
+//    @Override
+//    protected Fragment createFragment(){
+//        return new MovieDetailsFragment();
+//
+//    }
+//
+//    @Override
+//    protected int getLayoutResID(){
+//
+//        return R.layout.activity_details;
+//    }
+//
 
 }
